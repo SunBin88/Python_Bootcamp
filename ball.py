7fa51009a4a8d3305d250f43e317dff7d2f6a9d3
SunBin88 <sunbiny77@naver.com>
2022-12-12 23:05:14 +0900

    2022/12/12

---
+++ ball.py
@@ -0,0 +1,22 @@
+from turtle import Turtle
+
+
+class Ball(Turtle):
+    def __init__(self):
+        super().__init__()
+        self.shape("circle")
+        self.color("white")
+        self.penup()
+        self.x_move = 8
+        self.y_move = 8
+
+    def move(self):
+        new_x = self.xcor() + self.x_move
+        new_y = self.ycor() + self.y_move
+        self.goto(new_x, new_y)
+
+    def bounce(self):
+        self.y_move *= -1
+
+    def hit(self):
+        self.x_move *= -1
